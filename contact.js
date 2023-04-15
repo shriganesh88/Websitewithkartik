ffcefcd5148ad38e15b1e5071105245c6ed8ae30
shriganesh88 <123148588+shriganesh88@users.noreply.github.com>
2023-04-15 13:30:15 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,7 @@
+<form id="contactForm" method="POST" action="sendmail.php">
+  <input type="text" name="name" placeholder="Your Name" required>
+  <input type="email" name="email" placeholder="Your Email" required>
+  <input type="tel" name="phone" placeholder="Your Phone Number">
+  <textarea name="message" placeholder="Your Message" required></textarea>
+  <button type="submit">Submit</button>
+</form>
